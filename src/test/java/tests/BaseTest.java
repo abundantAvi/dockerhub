c63fed6928fca43a225a724095a3d4d08f6751da
avirajBoxture <101553936+avirajBoxture@users.noreply.github.com>
2022-08-18 23:34:18 +0530

    baseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,10 +1,20 @@
 package tests;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.BeforeTest;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.Properties;
 
 
@@ -15,8 +25,44 @@ public class BaseTest {
     private Properties prop;
 
     @BeforeTest
-    public void setupDriver() throws IOException {
+    public void setupDriver(ITestContext ctx, String testCaseName)throws IOException{
 
+//BROWSER=>chrome/firefox
+//HUB_HOST=>localhost/10.0.1.3/hostname
+
+//Stringhost;
+        MutableCapabilities dc;
+
+//Propertiesproperties=newProperties();
+//Stringfile="data.properties";
+//InputStreaminputstream=getClass().getClassLoader().getResourceAsStream(file);
+//properties.load(inputstream);
+        String browserName=System.getProperty("BROWSER");
+        String host=System.getProperty("HUB_HOST");
+
+
+        if(browserName!=null&&
+                browserName.equalsIgnoreCase("firefox")){
+            System.out.println("firefoxloading...");
+            dc=new FirefoxOptions();
+        }else{
+            dc=new ChromeOptions();
+            System.out.println("chromeloading...");
+
+        }
+
+
+        String testName=ctx.getCurrentXmlTest().getName();
+
+        String completeUrl="http://"+host+":4444/wd/hub";
+        System.out.println(host);
+        dc.setCapability("test",testName);
+        this.driver=new RemoteWebDriver(new URL(completeUrl),dc);
+
+        TakesScreenshot ts=(TakesScreenshot)driver;
+        File source=ts.getScreenshotAs(OutputType.FILE);
+        String destinationFile=System.getProperty("user.dir")+"\\reports\\"+testCaseName+".png";
+        FileUtils.copyFile(source,new File(destinationFile));
     }
 
 
