f8c2c21122929d69affef6427b39bae8d5ee9b46
avirajBoxture <101553936+avirajBoxture@users.noreply.github.com>
2022-08-23 16:21:41 +0530

    changes

--- src/test/java/com/sel/tests/Boxture_SalesOrder.java
+++ src/test/java/com/sel/tests/Boxture_SalesOrder.java
@@ -64,6 +64,8 @@ public class Boxture_SalesOrder extends BaseTest {
         createPage.confirmOrderClick();
         createPage.pickOnClick();
         createPage.pickListClick();
+        String picker = createPage.orderNumGenerated();
+        System.out.println(picker);
         createPage.mobileWebsite();
         createPage.scanTote(tote);
         createPage.pickAfterScan();
