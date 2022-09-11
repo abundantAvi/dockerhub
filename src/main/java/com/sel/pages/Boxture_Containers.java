84b93e16620d4c18e55051c6b19dae66fec76b04
avirajBoxture <101553936+avirajBoxture@users.noreply.github.com>
2022-09-11 18:28:50 +0530

    create container

---
+++ src/main/java/com/sel/pages/Boxture_Containers.java
@@ -0,0 +1,75 @@
+package com.sel.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Boxture_Containers {
+     public WebDriver driver;
+    private final WebDriverWait wait;
+
+    @FindBy(linkText = "Containers")
+    private WebElement containers;
+    @FindBy(linkText = "Create")
+    private WebElement create;
+    @FindBy(xpath = "//input[@placeholder='Packing materialabc']")
+    private WebElement packingMaterial;
+    @FindBy(xpath = "//input[@placeholder='Bin location']")
+    private WebElement binLoc;
+    @FindBy(xpath = "//button[normalize-space()='Create Container']")
+    private WebElement createContainer;
+
+
+    @FindBy(xpath = "//span[normalize-space()='View notifications']")
+    private WebElement notificationCntainer;
+    @FindBy(css = ".pnotify-text")
+    private WebElement a;
+
+    @FindBy(xpath = "//main/div/div/div/div/div/div/div[2]/h3/satis-copyable")
+    private WebElement containerNum;
+
+    public Boxture_Containers(WebDriver driver) {
+
+        this.driver = driver;
+       wait = new WebDriverWait(driver, 30);
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setContainers() {
+        wait.until(ExpectedConditions.visibilityOf(this.containers));
+        containers.click();
+    }
+    public void setCreate() {
+        wait.until(ExpectedConditions.visibilityOf(this.create));
+        create.click();
+    }
+    public void setPackingMaterial(String packing_Material) throws InterruptedException {
+
+        wait.until(ExpectedConditions.visibilityOf(this.packingMaterial));
+        packingMaterial.click();
+        packingMaterial.clear();
+        packingMaterial.sendKeys(packing_Material);
+        Thread.sleep(3000);
+    }
+
+    public void setBinLoc(String bin) throws InterruptedException {
+        this.wait.until(ExpectedConditions.visibilityOf(this.binLoc));
+        binLoc.click();
+        binLoc.sendKeys(bin);
+        Thread.sleep(3000);
+
+    }
+
+    public void setCreateContainer() throws InterruptedException {
+        createContainer.click();
+        Thread.sleep(3000);
+    }
+
+    public String setNotificationCntainer() throws InterruptedException {
+        String b = a.getText().substring(10,18);
+        return b;
+    }
+}
---
+++ src/test/java/com/sel/tests/Boxture_ContainersCreation.java
@@ -0,0 +1,51 @@
+package com.sel.tests;
+
+import com.sel.pages.Boxture_Containers;
+import com.sel.pages.Boxture_LoginPage;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+import tests.BaseTest;
+
+import java.awt.*;
+
+public class Boxture_ContainersCreation extends BaseTest {
+
+    private String username;
+    private String password;
+    private String containerNumber;
+
+    Boxture_Containers containers;
+
+    @BeforeTest
+    @Parameters({"username1", "password"})
+    public void setupParameters(String username1, String password) {
+        this.username = username1;
+        this.password = password;
+
+    }
+    @Test
+    public void containerCreate() throws AWTException, InterruptedException {
+
+        Boxture_LoginPage loginPage = new Boxture_LoginPage(driver);
+        loginPage.loginUrl();
+        Thread.sleep(3000);
+        loginPage.username_enter(username);
+        loginPage.password_enter(password);
+        loginPage.login_buttonClick();
+    }
+    @Test(dependsOnMethods = {"containerCreate"})
+    public void container() throws InterruptedException {
+        containers = new Boxture_Containers(driver);
+        containers.setContainers();
+        containers.setCreate();
+        containers.setPackingMaterial("box 1");
+        containers.setBinLoc("AVITESTBIN123");
+        containers.setCreateContainer();
+        containers.setNotificationCntainer();
+        containerNumber = containers.setNotificationCntainer();
+        System.out.println(containerNumber);
+
+    }
+
+}
