e1db45392dffc15a4d254bedc222f39edc8a96f9
avirajBoxture <101553936+avirajBoxture@users.noreply.github.com>
2022-09-27 08:11:45 +0530

    Update Boxture_Purchase.java

--- src/main/java/com/sel/pages/Boxture_Purchase.java
+++ src/main/java/com/sel/pages/Boxture_Purchase.java
@@ -159,6 +159,7 @@ public class Boxture_Purchase {
         System.out.println("Box 1 - Prod");
         Thread.sleep(2000);
         this.finalReceive.click();
+        Thread.sleep(3000);
         this.driver.navigate().refresh();
         this.wait.until(ExpectedConditions.visibilityOf(this.status));
         System.out.println("Purchase Completed");
